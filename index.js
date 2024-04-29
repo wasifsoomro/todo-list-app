790650c947292eea595e7c3acde2c090078680f6
Wasif Soomro <wasifsoomro761@gmail.com>
2024-04-29 11:40:27 +0500

    second coomit

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
+#!/usr/bin/env node
 import inquirer from "inquirer";
-let currencies = {
-    USD: 1,
-    PKR: 280,
-    EURO: 0.94,
-    SAU: 3.75,
-    AED: 3.67
-};
-async function currencyConverter() {
-    let answer = await inquirer.prompt([{
-            type: "list",
-            name: "from",
-            message: "Choose Currency you want to Convert from",
-            choices: ["USD", "PKR", "EURO", "SAU", "AED"]
-        },
-        {
-            type: "list",
-            name: "to",
-            message: "Choose currency you want to convert into",
-            choices: ["USD", "PKR", "EURO", "SAU", "AED"]
-        },
-        {
-            type: "input",
-            name: "EnterAmount",
-            message: "Enter Amount ",
+let todo = [];
+async function todoList(todo) {
+    do {
+        let selectTodo = await inquirer.prompt([{
+                type: "list",
+                name: "selectOption",
+                message: "Select any one option",
+                choices: ["add", "update", "view", "delete", "exit"]
+            }]);
+        if (selectTodo.selectOption == "add") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                name: "addOption",
+                message: "Write anything you want to add in list"
+            });
+            todo.push(addTodo.addOption);
+            todo.forEach(todo => console.log(todo));
         }
-    ]);
-    let amountFrom = currencies[answer.from];
-    let amountTo = currencies[answer.to];
-    let amount = parseFloat(answer.EnterAmount);
-    let convertedAmount = (amount / amountFrom) * amountTo;
-    console.log(convertedAmount.toFixed(2));
-    console.log(amountFrom);
-    console.log(amountTo);
-    console.log(amount);
+        if (selectTodo.selectOption == "update") {
+            let updateTodo = await inquirer.prompt([{
+                    type: "list",
+                    name: "Updateitem",
+                    message: "Select any item you want to update",
+                    choices: todo
+                },
+                {
+                    type: "input",
+                    name: "writeUpdateOne",
+                    message: "write new item"
+                }
+            ]);
+            let indexNumber = todo.indexOf(updateTodo.Updateitem);
+            let updateNewOne = updateTodo.writeUpdateOne;
+            todo.splice(indexNumber, 1, updateNewOne);
+            todo.forEach(todo => console.log(todo));
+        }
+        if (selectTodo.selectOption == "view") {
+            console.log("★★★★★★★★★★★Todo List★★★★★★★★★★★★★");
+            todo.forEach(todo => console.log(todo));
+            console.log("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★");
+        }
+        if (selectTodo.selectOption == "delete") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                name: "deleteitem",
+                message: "Select any item you want to delete",
+                choices: todo
+            });
+            let deleteIndex = todo.indexOf(deleteTodo.deleteitem);
+            todo.splice(deleteIndex, 1);
+            todo.forEach(todo => console.log(todo));
+        }
+        if (selectTodo.selectOption === "exit") {
+            console.log("Closing the application...");
+            process.exit(0);
+        }
+    } while (true);
 }
-currencyConverter();
+todoList(todo);
